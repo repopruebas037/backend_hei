534610131ba5bc1a228b2ec8a06118ac267440dc
johnnyjojoa7 <johnnyjojoa7@gmail.com>
2025-03-07 22:04:00 -0500

    update

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,6 +1,7 @@
-from django.http.response import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_POST
 import json
+import requests
 
 import chatbot.services as services
 
@@ -26,3 +27,49 @@ def chatbot(request):
     chat_response = services.get_chat_response(user_prompt)
 
     return JsonResponse({"message": chat_response}, status=200)
+
+VERIFICATION_TOKEN = "abcdefg12345"
+
+@csrf_exempt
+def verify_whatsapp(request):
+    if request.method == "GET":
+        print("verifying")
+        hub_mode = request.GET.get("hub.mode")
+        hub_challenge = request.GET.get("hub.challenge")
+        hub_verify_token = request.GET.get("hub.verify_token")
+
+        if hub_mode == "subscribe" and hub_verify_token == VERIFICATION_TOKEN:
+            return HttpResponse(hub_challenge) 
+        return JsonResponse({"error": "Invalid verification token"}, status=403)
+    
+    if request.method == "POST":
+        data = json.loads(request.body)
+        print(data)
+        return JsonResponse({"status": "Mensaje recibido"}, status=200)
+
+    return JsonResponse({"error": "Método no permitido"}, status=405)
+
+
+WHATSAPP_API_KEY = "EAArts55hNvMBOZC8QunzoeW9xQdKCArk9FaquRZBKBxrhxdLBnVTtM1UlVlcnmYG4I0pzu3ZBKKllBQacUBX1YirBf6ZBeo1VZCGv54QiNekenEz6GLYLGiJgApUWs2WB886NOusfMnNOZBIKCCOOrWdPOHOrZAqdmaOVdZCPRCvZCb7oQyUiSMKj9SLzIZCCe2uGD0MfGj7AjbmS2LqZAZBMvU1weFFTfqxZB5jGpdAZD"
+
+
+@csrf_exempt
+def send_whatsapp_message(to):
+    url = "https://graph.facebook.com/v22.0/621653484354259/messages"
+    headers = {
+        "Authorization": f"Bearer " + WHATSAPP_API_KEY,
+        "Content-Type": "application/json"
+    }
+    
+    data = {
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": to,
+        "type": "text",
+        "text": {
+            "body": "Hola"
+        }
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    return response.json()
